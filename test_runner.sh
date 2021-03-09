7f3e900e6a3a27bd531527d3f05703d9bf4a6511
Matthew Feickert <matthew.feickert@cern.ch>
2021-03-09 09:36:17 -0600

    Have CI uses tests

--- test_runner.sh
+++ test_runner.sh
@@ -2,5 +2,5 @@
 
 docker run --rm \
 	-v "${PWD}":"${PWD}" \
-	scailfin/madgraph5-amc-nlo:debug-local "cd ${PWD}/tests; pwd; bash tests.sh"
+	scailfin/madgraph5-amc-nlo:debug-local "cd ${PWD}/tests; bash tests.sh"
 tree -L 2 tests/
