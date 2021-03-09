0940f225213102ac7232bf8068e2ff909ff3ef16
Matthew Feickert <matthew.feickert@cern.ch>
2021-03-09 09:39:43 -0600

    Have test runner use latest

--- test_runner.sh
+++ test_runner.sh
@@ -2,5 +2,5 @@
 
 docker run --rm \
 	-v "${PWD}":"${PWD}" \
-	scailfin/madgraph5-amc-nlo:debug-local "cd ${PWD}/tests; bash tests.sh"
+	scailfin/madgraph5-amc-nlo:mg5_amc2.9.2-python3 "cd ${PWD}/tests; bash tests.sh"
 tree -L 2 tests/
