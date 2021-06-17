67f145b18d9f114a32930437b032857d41550bc4
Matthew Feickert <matthew.feickert@cern.ch>
2021-06-17 18:25:04 -0500

    fix: Set default user as root and place libraries on LD_LIBRARY_PATH (#39)
    * Set default user as root
       - If default user is non-root they will have no write capabilities to volume mounts
       - c.f. https://github.com/moby/moby/issues/2259
       - Partially reverts PR #22
    * Place user installed libraries (in /usr/local/venv/lib) on LD_LIBRARY_PATH so they are findable by Python
    * Add Python import tests for LHAPDF, PYTHIA8, and FastJet

--- tests/test_import.py
+++ tests/test_import.py
@@ -2,3 +2,21 @@ def test_import_madgraph():
     import madgraph
 
     assert madgraph
+
+
+def test_import_lhapdf():
+    import lhapdf
+
+    assert lhapdf
+
+
+def test_import_pythia():
+    import pythia8
+
+    assert pythia8
+
+
+def test_import_fastjet():
+    import fastjet
+
+    assert fastjet
